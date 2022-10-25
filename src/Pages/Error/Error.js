2497fc4ad7fedcbed77694c1058dd8ca81a289a7
AmanatUMahmud <amanatmahmud@gmail.com>
2022-10-25 17:21:11 +0600

    error page responsive

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const Error = () => {
     return (
         <div className='text-center'>
-            <h1 className='lg:text-9xl mt-56'>404</h1>
-            <p className='mt-4'><span className='lg:text-5xl'>O</span><span className='lg:text-4xl'>ops....</span></p>
-            <p className='lg:text-3xl lg:mx-48 my-4'>The page requested couldn't be found. This could be a spelling error in the URL or a removed page.</p>
-            <Link to='/'><button className="btn bg-[#00b58b] text-white border-none">Back to Home Page</button></Link>
+            <h1 className='text-5xl md:text-9xl lg:mt-56 mt-40'>404</h1>
+            <p className='mt-4'><span className='lg:text-5xl md:text-4xl text-3xl'>O</span><span className='lg:text-4xl md:text-3xl text-2xl'>ops....</span></p>
+            <p className='lg:text-3xl text-xl md:text-2xl lg:mx-48 mx-20 my-4'>The page requested couldn't be found. This could be a spelling error in the URL or a removed page.</p>
+            <Link to='/'><button className="btn bg-[#00b58b] text-white border-none ">Back to Home Page</button></Link>
         </div>
     );
 };
