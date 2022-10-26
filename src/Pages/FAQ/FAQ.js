a2a246d24b8e8f7ea2cfc470e56d5d7479583231
AmanatUMahmud <amanatmahmud@gmail.com>
2022-10-26 20:36:10 +0600

    FAQ section done

--- src/Pages/FAQ/FAQ.js
+++ src/Pages/FAQ/FAQ.js
@@ -6,40 +6,100 @@ const FAQ = () => {
             <div className='my-5 w-3/4 mx-auto'>
                 <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box dark:bg-black">
                     <div className="collapse-title text-xl font-medium">
-                        Focus me to see content
+                    What are the options of buying courses?
                     </div>
                     <div className="collapse-content dark:bg-black">
-                        <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                        <p>You can buy our courses using any bank. You can also pay using paypal, payoneer, visa card, master card.</p>
                     </div>
                 </div>
             </div>
             <div className='mb-5 w-3/4 mx-auto'>
                 <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box dark:bg-black">
                     <div className="collapse-title text-xl font-medium">
-                        Focus me to see content
+                    Are the courses lifetime accessable?
                     </div>
                     <div className="collapse-content dark:bg-black">
-                        <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                        <p>Yes.</p>
                     </div>
                 </div>
             </div>
             <div className='mb-5 w-3/4 mx-auto'>
                 <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box dark:bg-black">
                     <div className="collapse-title text-xl font-medium">
-                        Focus me to see content
+                    Do you offer any specialized course?
                     </div>
                     <div className="collapse-content dark:bg-black">
-                        <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                        <p>We are planing on bringing specialized course. Subscribe to our newsletter and get notified when we start it.</p>
                     </div>
                 </div>
             </div>
             <div className='mb-5 w-3/4 mx-auto'>
                 <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box dark:bg-black">
                     <div className="collapse-title text-xl font-medium">
-                        Focus me to see content
+                    Do you issue refunds?
                     </div>
                     <div className="collapse-content dark:bg-black">
-                        <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+                        <p>Yes we do if you aplly within 1 hour.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='mb-5 w-3/4 mx-auto'>
+                <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box dark:bg-black">
+                    <div className="collapse-title text-xl font-medium">
+                    Do you offer certificates for all course?
+                    </div>
+                    <div className="collapse-content dark:bg-black">
+                        <p>Yes.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='mb-5 w-3/4 mx-auto'>
+                <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box dark:bg-black">
+                    <div className="collapse-title text-xl font-medium">
+                    Can working adults do your course?
+                    </div>
+                    <div className="collapse-content dark:bg-black">
+                        <p>Of course. Our couses are self paced and course content hours are given. So you can easily find out how much time it will take you to complete a course.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='mb-5 w-3/4 mx-auto'>
+                <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box dark:bg-black">
+                    <div className="collapse-title text-xl font-medium">
+                    How long does it take to complete a course?
+                    </div>
+                    <div className="collapse-content dark:bg-black">
+                        <p>Depends on the time you give and and the duration of the course.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='mb-5 w-3/4 mx-auto'>
+                <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box dark:bg-black">
+                    <div className="collapse-title text-xl font-medium">
+                    Can I buy a sample course?
+                    </div>
+                    <div className="collapse-content dark:bg-black">
+                        <p>You can't buy any sample course. But you can watch 1/2 videos free of cost of any course.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='mb-5 w-3/4 mx-auto'>
+                <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box dark:bg-black">
+                    <div className="collapse-title text-xl font-medium">
+                    Who to contact when I face any problem in finishing or understanding the course?
+                    </div>
+                    <div className="collapse-content dark:bg-black">
+                        <p>You can contat with the instructor. Contact section will be available after enrolling the course.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='mb-5 w-3/4 mx-auto'>
+                <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box dark:bg-black">
+                    <div className="collapse-title text-xl font-medium">
+                    Do you offer any offline course?
+                    </div>
+                    <div className="collapse-content dark:bg-black">
+                        <p>No.</p>
                     </div>
                 </div>
             </div>
