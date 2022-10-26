5a2d1d4b8f5c81433a4a3d7f1d04d8463ef84a6f
AmanatUMahmud <amanatmahmud@gmail.com>
2022-10-26 21:28:44 +0600

    Blog done

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Blog = () => {
     return (
         <div>
-            <h1>Inside blog</h1>
+            <div className='mx-20 my-10 border rounded-2xl dark:border-black dark:text-black'>
+                <div className="card bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">What is cors?</h2>
+                        <p>CORS stands for Cross-Origin Resource Sharing. It allows us to relax the security applied to an API. This is done by bypassing the Access-Control-Allow-Origin headers, which specify which origins can access the API. In other words, CORS is a browser security feature that restricts cross-origin HTTP requests with other servers and specifies which domains access your resources. <a href="https://www.section.io/engineering-education/how-to-use-cors-in-nodejs-with-express/" target="_blank" rel="noreferrer" className="text-[#00b58b] underline hover:underline-offset-4">Read more</a></p>
+                    </div>
+                </div>
+            </div>
+            <div className='mx-20 mb-10 border rounded-2xl
+            dark:border-black dark:text-black'>
+                <div className="card bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">Why are you using firebase? What other options do you have to implement authentication?</h2>
+                        <p>Google Firebase is an application development platform that allows developers to create iOS, Android, and Web apps.Google Firebase offers many features that pitch it as the go-to backend development tool for web and mobile apps. It reduces development workload and time. And it's a perfect prototyping tool. Firebase is simple, lightweight, friendly, and industrially recognized. <span className='font-bold'>We use firebase because of storage,hosting,authentication,Firebase ML and built in push notification.</span> Other options are Back4App, Backendless, Kuzzle, Pubnub, Kumulos, Appwrite, Deployd, NHost, AWS Amplify, Heroku etc.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='mx-20 mb-10 border rounded-2xl
+            dark:border-black dark:text-black'>
+                <div className="card bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">How does the private route work?</h2>
+                        <p>Private Routes in React Router (also called Protected Routes) require a user being authorized to visit a route. So if a user is not authorized for a specific page, they cannot access it. The most common example is authentication in a React application where a user can only access the protected pages when they are authorized (which means in this case being authenticated). Authorization goes beyond authentication though. For example, a user can also have roles and permissions which give a user access to specific areas of the application.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='mx-20 mb-10 border rounded-2xl
+            dark:border-black dark:text-black'>
+                <div className="card bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">What is Node? How does Node work?</h2>
+                        <p>Node.js is a platform built on Chrome's JavaScript runtime for easily building fast and scalable network applications. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.Node.js is open source, cross-platform runtime environment for developing server-side and networking applications. Node.js applications are written in JavaScript, and can be run within the Node.js runtime on OS X, Microsoft Windows, and Linux.Node is completely event-driven. Basically the server consists of one thread processing one event after another. A new request coming in is one kind of event. The server starts processing it and when there is a blocking IO operation, it does not wait until it completes and instead registers a callback function. The server then immediately starts to process another event (maybe another request). When the IO operation is finished, that is another kind of event, and the server will process it (i.e. continue working on the request) by executing the callback as soon as it has time.So the server never needs to create additional threads or switch between threads, which means it has very little overhead. If you want to make full use of multiple hardware cores, you just start multiple instances of node.js</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
