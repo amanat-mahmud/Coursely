e903113273bc3a36e717fa0a49bb58fb3d7e802b
AmanatUMahmud <amanatmahmud@gmail.com>
2022-12-03 09:21:47 +0600

    learn from us added

--- src/Pages/Home/LearnCard.js
+++ src/Pages/Home/LearnCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const LearnCard = ({title,description,image}) => {
     return (
-        <div className="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img src={image} className='w-full' alt="Album"/></figure>
+        <div className="card lg:card-side bg-base-100 shadow-xl border-2 border-white hover:border-2 hover:border-[#00b58b]">
+  <figure><img src={image} className='w-4/5' alt="Album"/></figure>
   <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
+    <h2 className="card-title font-bold">{title}</h2>
+    <p>{description}</p>
    
   </div>
 </div>
--- src/Pages/Home/LearnFromUs.js
+++ src/Pages/Home/LearnFromUs.js
@@ -7,10 +7,16 @@ const LearnFromUs = () => {
     return (
         <div className='mb-10 mx-10'>
             <h1 className='text-5xl font-bold mb-10'>Why learn from us?</h1>
-            <div className='grid grid-cols-3'>
-            <LearnCard title={"Skill"}
-            description={"Test"}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            <LearnCard title={"Get Skilled"}
+            description={"from thousands of quality courses.Designed by experts."}
             image={skillUp}></LearnCard>
+            <LearnCard title={"Expert Tutors"}
+            description={"All our courses are designed by experts so that you can get best of the best."}
+            image={expert}></LearnCard>
+            <LearnCard title={"Certificate"}
+            description={"After completing any course with minimum required marks you get certificate."}
+            image={certificate}></LearnCard>
             </div>
             
         </div>
